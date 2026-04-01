df6845f7d1556c89bd86de292fca1456216d1043
Kevin <5387662@upjs.sk>
2026-04-01 10:58:05 +0200

    added more types of spawnrate of obstacles

--- src/obstacle.rs
+++ src/obstacle.rs
@@ -1,14 +1,22 @@
 use bevy::prelude::*;
 use rand::{RngExt};
-use crate::difficulty::Difficulty;
 
 use crate::constants::*;
+use crate::difficulty::Difficulty;
 use crate::game_state::GameState;
+use crate::score::Score;
 
 #[derive(Clone, Copy, Debug)]
 pub enum ObstacleType {
     SmallCactus,
     LargeCactus,
+}
+
+#[derive(Clone, Copy, Debug)]
+pub enum SpawnPattern {
+    Close,
+    Normal,
+    Far,
 }
 
 #[derive(Component)]
@@ -38,6 +46,16 @@ impl ObstacleType {
     }
 }
 
+impl SpawnPattern {
+    pub fn interval_range(&self) -> (f32, f32) {
+        match self {
+            SpawnPattern::Close => (1.10, 1.35),
+            SpawnPattern::Normal => (1.45, 1.85),
+            SpawnPattern::Far => (1.95, 2.35),
+        }
+    }
+}
+
 fn random_obstacle_type() -> ObstacleType {
     let mut rng = rand::rng();
 
@@ -48,9 +66,45 @@ fn random_obstacle_type() -> ObstacleType {
     }
 }
 
-fn random_spawn_interval() -> f32 {
+fn choose_spawn_pattern(score: f32) -> SpawnPattern {
     let mut rng = rand::rng();
-    rng.random_range(1.2..=2.0)
+
+    if score < 800.0 {
+        if rng.random_bool(0.75) {
+            SpawnPattern::Normal
+        } else {
+            SpawnPattern::Far
+        }
+    } else if score < 2000.0 {
+        let roll = rng.random_range(0..100);
+
+        if roll < 20 {
+            SpawnPattern::Close
+        } else if roll < 75 {
+            SpawnPattern::Normal
+        } else {
+            SpawnPattern::Far
+        }
+    } else {
+        let roll = rng.random_range(0..100);
+
+        if roll < 35 {
+            SpawnPattern::Close
+        } else if roll < 80 {
+            SpawnPattern::Normal
+        } else {
+            SpawnPattern::Far
+        }
+    }
+}
+
+fn random_spawn_interval(pattern: SpawnPattern, speed: f32) -> f32 {
+    let mut rng = rand::rng();
+    let (min_time, max_time) = pattern.interval_range();
+    
+    let speed_factor = (BASE_SPEED / speed).clamp(0.65, 1.0);
+
+    rng.random_range((min_time * speed_factor)..=(max_time * speed_factor))
 }
 
 pub fn spawn_obstacle(
@@ -58,6 +112,8 @@ pub fn spawn_obstacle(
     time: Res<Time>,
     mut spawn_timer: ResMut<ObstacleSpawnTimer>,
     game_state: Res<GameState>,
+    score: Res<Score>,
+    difficulty: Res<Difficulty>,
 ) {
     if *game_state != GameState::Running {
         return;
@@ -67,7 +123,10 @@ pub fn spawn_obstacle(
 
     if spawn_timer.timer >= spawn_timer.next_spawn_time {
         spawn_timer.timer = 0.0;
-        spawn_timer.next_spawn_time = random_spawn_interval();
+
+        let pattern = choose_spawn_pattern(score.distance);
+        spawn_timer.next_spawn_time =
+            random_spawn_interval(pattern, difficulty.obstacle_speed);
 
         let obstacle_type = random_obstacle_type();
         let size = obstacle_type.size();
